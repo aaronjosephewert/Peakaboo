faa6a1babd64996ad995f83691173ab545df5de5
Aaron Ewert <aaronjewert@gmail.com>
2021-10-19 10:16:47 -0700

    add jQuery effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
+    $(".hide-image").toggle();
+    $(".show-image").toggle();
   });
 });
